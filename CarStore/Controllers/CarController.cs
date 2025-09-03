97b78686dba7310990f8ebb11db5043b67bcad51
Oleksandr Panchuk <alekssen2008@gmail.com>
2025-09-03 11:07:43 +0300

    add create and edit view, add js func which ask user before deleting car, change default start controller, fix bugs...

--- CarStore/Controllers/CarController.cs
+++ CarStore/Controllers/CarController.cs
@@ -1,7 +1,9 @@
 ﻿using DataAccess;
 using DataAccess.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
+using System.Linq;
 
 namespace CarStore.Controllers
 {
@@ -36,6 +38,76 @@ namespace CarStore.Controllers
             db.SaveChanges();
             return RedirectToAction(nameof(Index));
         }
+        public IActionResult Create()
+        {
+            GetViewBags();
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Car car)
+        {
+            car = LinkCarDependencies(car);
+
+            db.Cars.Add(car);
+            db.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private Car LinkCarDependencies(Car car)
+        {
+            string Brand = car.Brand.Name.Trim();
+            var findBrandObj = db.Brands.FirstOrDefault(c => c.Name == Brand);
+            if (findBrandObj != null)
+            {
+                car.Brand = findBrandObj;
+            }
+
+            string Country = car.Country.Name.Trim();
+            var findCountryObj = db.Countries.FirstOrDefault(c => c.Name == Country);
+            if (findCountryObj != null)
+            {
+                car.Country = findCountryObj;
+            }
+            else
+            {
+                db.Countries.Add(new Country() { Name = Country });
+                db.SaveChanges();
+                car.Country = db.Countries.FirstOrDefault(c => c.Name == Country)!;
+            }
+
+            string Region = car.Region.Name.Trim();
+            var findRegionObj = db.Regions.FirstOrDefault(c => c.Name == Region);
+            if (findRegionObj != null)
+            {
+                car.Region = findRegionObj;
+            }
+            else
+            {
+                car.Region.Country = car.Country;
+            }
+            return car;
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var Car = GetCars().FirstOrDefault(c => c.Id == id);
+            if (Car == null)
+            {
+                return NotFound();
+            }
+            GetViewBags();
+            return View(Car);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Car car)
+        {
+            car = LinkCarDependencies(car);
+            db.Cars.Update(car);
+            db.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
         public IQueryable<Car> GetCars()
         {
             return db.Cars
@@ -45,6 +117,15 @@ namespace CarStore.Controllers
                 .Include(c => c.FuelType)
                 .Include(c => c.Region)
                 .Include(c => c.Country);
+        }
+        public void GetViewBags()
+        {
+            ViewBag.Brands = new SelectList(db.Brands.ToList(), nameof(Brand.Id), nameof(Brand.Name));
+            ViewBag.Colors = new SelectList(db.Colors.ToList(), nameof(Color.Id), nameof(Color.Name));
+            ViewBag.CarTypes = new SelectList(db.CarTypes.ToList(), nameof(CarType.Id), nameof(CarType.Name));
+            ViewBag.Countries = new SelectList(db.Countries.ToList(), nameof(Country.Id), nameof(Country.Name));
+            ViewBag.Regions = new SelectList(db.Regions.ToList(), nameof(Region.Id), nameof(Region.Name));
+            ViewBag.FuelTypes = new SelectList(db.FuelTypes.ToList(), nameof(FuelType.Id), nameof(FuelType.Name));
         }
     }
 }
--- CarStore/wwwroot/js/site.js
+++ CarStore/wwwroot/js/site.js
@@ -2,3 +2,12 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+
+const deleteAlert = (id) => {
+    
+    if (confirm("Are you sure you want to delete this car?")) {
+        window.location.href = `/Car/Delete/${id}`;
+    }
+}
